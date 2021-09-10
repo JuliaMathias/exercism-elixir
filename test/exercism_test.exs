c95b2a793a043522524909dc57a094e91ee74e38
JuliaMathias <juliamss@gmail.com>
2021-09-10 15:19:45 -0300

    test: kitchen calculator

--- test/exercism_test.exs
+++ test/exercism_test.exs
@@ -1,8 +1,4 @@
 defmodule ExercismTest do
   use ExUnit.Case
   doctest Exercism
-
-  test "greets the world" do
-    assert Exercism.hello() == :world
-  end
 end
---
+++ test/pattern_matching/kitchen_calculator_test.exs
@@ -0,0 +1,5 @@
+defmodule Exercism.PatternMatching.KitchenCalculatorTest do
+  use ExUnit.Case, async: true
+  alias Exercism.PatternMatching.KitchenCalculator
+  doctest Exercism.PatternMatching.KitchenCalculator, import: true
+end

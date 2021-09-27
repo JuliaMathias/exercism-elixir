6b6e15392c93a10428bf80248bd3bbe75418cc1f
JuliaMathias <juliamss@gmail.com>
2021-09-27 14:30:03 -0300

    wip: username

---
+++ lib/case/username.ex
@@ -0,0 +1,36 @@
+defmodule Exercism.Case.Username do
+  @moduledoc """
+  You are working as a system administrator for a big company in Munich, Germany. One of your responsibilities is managing email accounts.
+
+  You have been hearing complaints from people saying they are unable to write emails to Mr. Müller. You quickly realize that most of the company uses an old email client that doesn't recognize müller@firma.de as a valid email address because of the non-Latin character.
+
+  Telling people to give up their favorite old email client is a lost battle, so you decide to create sanitized aliases for all email accounts.
+  """
+
+  @doc """
+  Implement the sanitize/1 function. It should accept a username as a charlist and return the username with all characters but lowercase letters removed.
+
+  ## Examples
+    iex> Username.sanitize('schmidt1985')
+    'schmidt'
+    iex> Username.sanitize('mark_fischer$$$')
+    'mark_fischer'
+  """
+  def sanitize(username) do
+    # ä becomes ae
+    # ö becomes oe
+    # ü becomes ue
+    # ß becomes ss
+
+    # Please implement the sanitize/1 function
+    username
+    |> List.to_string()
+    |> String.replace(~r"[0-9 /]", "")
+    |> String.to_charlist()
+
+    # case head do
+    #   head when head in 97..122 -> sanitize([tail])
+
+    # end
+  end
+end

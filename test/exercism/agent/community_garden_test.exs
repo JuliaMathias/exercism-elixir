e07ef784178fd88301374c32c69e3cb02c5d6574
JuliaMathias <juliamss@gmail.com>
2021-11-08 20:33:31 -0300

    test: community garden

---
+++ test/exercism/agent/community_garden_test.exs
@@ -0,0 +1,6 @@
+defmodule Exercism.Agent.CommunityGardenTest do
+  use ExUnit.Case
+  alias Exercism.Agent.CommunityGarden
+  alias Exercism.Agent.Plot
+  doctest Exercism.Agent.CommunityGarden
+end

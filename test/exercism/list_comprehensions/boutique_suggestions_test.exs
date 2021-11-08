0a867651f4de14104513802f42761307fd021827
JuliaMathias <juliamss@gmail.com>
2021-11-08 14:43:37 -0300

    test: boutique suggestions

---
+++ test/exercism/list_comprehensions/boutique_suggestions_test.exs
@@ -0,0 +1,5 @@
+defmodule Exercism.ListComprehensions.BoutiqueSuggestionsTest do
+  use ExUnit.Case
+  alias Exercism.ListComprehensions.BoutiqueSuggestions
+  doctest Exercism.ListComprehensions.BoutiqueSuggestions
+end

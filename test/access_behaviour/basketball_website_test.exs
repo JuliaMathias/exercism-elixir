40253d901414a4724d289eb6a426268c0c749cf2
JuliaMathias <juliamss@gmail.com>
2021-11-13 10:19:50 -0300

    test: basketball website

---
+++ test/access_behaviour/basketball_website_test.exs
@@ -0,0 +1,5 @@
+defmodule Exercism.AccessBehaviour.BasketballWebsiteTest do
+  use ExUnit.Case
+  alias Exercism.AccessBehaviour.BasketballWebsite
+  doctest Exercism.AccessBehaviour.BasketballWebsite
+end
